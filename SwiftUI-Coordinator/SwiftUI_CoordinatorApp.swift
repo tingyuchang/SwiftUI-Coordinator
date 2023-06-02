bf66b42269bbc93a3b5b1e26a41d57b56b492997
mattc <tingyugenius@gmail.com>
2023-06-02 14:18:06 +0800

    Initial Commit

---
+++ SwiftUI-Coordinator/SwiftUI_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoordinatorApp.swift
+//  SwiftUI-Coordinator
+//
+//  Created by matt on 2023/6/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
